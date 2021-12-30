df103d5ed15a17be9284cbc79f86f15c3c51a91c
Alper <alperemreeren@hotmail.com>
2021-12-30 18:40:20 +0300

    Added BSC Testnet and minor changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const ALPERSAN = artifacts.require('ALPERSAN')
-const Tether = artifacts.require('Tether')
+// const Tether = artifacts.require('Tether')
 const RWD = artifacts.require('RWD')
 const DecentralBank = artifacts.require('DecentralBank')
 
@@ -8,9 +8,9 @@ module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(ALPERSAN)
     const alpersan = await ALPERSAN.deployed()
 
-    //Deploy Mock Tether contract
-    await deployer.deploy(Tether)
-    const tether = await Tether.deployed()
+    // //Deploy Mock Tether contract
+    // await deployer.deploy(Tether)
+    // const tether = await Tether.deployed()
 
     // Deploy RWD Contract
     await deployer.deploy(RWD)
@@ -23,9 +23,9 @@ module.exports = async function(deployer, network, accounts) {
     // Transfer all RWD tokens to Decentral Bank
     await rwd.transfer(decentralBank.address, '1000000000000000000000000')
 
-    // Distribute 100 ALPERSAN tokens to investor
-    await alpersan.transfer(accounts[1], '100000000000000000000')
+    // Distribute 100 ALPERSAN tokens to project's own wallet
+    await alpersan.transfer("0x023D6FfcAA9b3B278fd8F805D116dAC6cAc39780", '100000000000000000000')
 
-    // Distribute 100 Tether tokens to investor
-    await tether.transfer(accounts[1], '100000000000000000000')
+    // // Distribute 100 Tether tokens to investor
+    // await tether.transfer(accounts[1], '100000000000000000000')
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,25 @@
 require('babel-register');
 require('babel-polyfill');
 
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const privatekey = '95ad9d78a9b2c6ee5bf59ffe9ca340bbcc497b91d95cb581460fd76da4726686';
+
 module.exports = {
     networks: {
-        development: {
-            host: '127.0.0.1',
-            port: '7545',
-            network_id: '*' // match any network
+        bsctestnet: {
+            provider: new HDWalletProvider(privatekey, 'https://data-seed-prebsc-1-s1.binance.org:8545/'),
+            port: '8545',
+            network_id: '*',
+            gas: 4500000,
+            gasPrice: 25000000000,
+            from: '0x023D6FfcAA9b3B278fd8F805D116dAC6cAc39780'
         },
+        // development: {
+        //     host: '127.0.0.1',
+        //     port: '7545',
+        //     network_id: '*' // match any network
+        // },
     },
     contracts_directory: './src/contracts',
     contracts_build_directory: './src/truffle_abis',

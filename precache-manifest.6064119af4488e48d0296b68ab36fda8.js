dd2758edf52924c53c935dd53b3fb76eb958ee89
Alper <alperemreeren@hotmail.com>
2021-12-30 18:44:01 +0300

    Updates

---
+++ precache-manifest.6064119af4488e48d0296b68ab36fda8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a436c833c100c0a0d11",
+    "url": "/alpersan-defi-stakingapp/static/js/main.4a436c83.chunk.js"
+  },
+  {
+    "revision": "9d6a5a3673a819fea963",
+    "url": "/alpersan-defi-stakingapp/static/css/1.38f1108a.chunk.css"
+  },
+  {
+    "revision": "9d6a5a3673a819fea963",
+    "url": "/alpersan-defi-stakingapp/static/js/1.9d6a5a36.chunk.js"
+  },
+  {
+    "revision": "5b0ab452f1d12657378d",
+    "url": "/alpersan-defi-stakingapp/static/js/runtime~main.5b0ab452.js"
+  },
+  {
+    "revision": "2dfccdbce3b320f8ced8442c44f8ed43",
+    "url": "/alpersan-defi-stakingapp/index.html"
+  }
+];
